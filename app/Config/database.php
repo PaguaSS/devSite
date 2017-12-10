88e9c7c2d89c988d1b05772583a5ffc9b1c58125
Anthony Paguaga <pagua210@gmail.com>
2017-12-09 20:12:06 -0600

    Initial configuration

---
+++ app/Config/database.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright     Copyright (c) Anthony Paguaga
+ * @package       app.Config
+ */
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'u991974310_rlpag',
+		'password' => 'xtycuvWV23',
+		'database' => 'u991974310_devsi',
+		'prefix' => ''
+	);
+
+}
